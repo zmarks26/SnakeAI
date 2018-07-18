c47114ab0feea62648ffeffd8a09c48aa7558c75
zmarks26 <38057435+zmarks26@users.noreply.github.com>
2018-07-17 21:49:31 -0500

    Update ai.js

--- ai.js
+++ ai.js
@@ -3,14 +3,14 @@ var direction = ['left', 'forward', 'right'];
 // initial model definition
 const model = tf.sequential();
 // Initialize output layer
-model.add(tf.layers.dense({units: 3, inputShape: [3], activation: 'linear'}));
+model.add(tf.layers.dense({units: 3, inputShape: [5], activation: 'linear'}));
 
 
 let directionTensor = tf.tensor1d(direction, 'int32');;
 directionTensor.dispose();
 
 
-const adamOpt = tf.train.adam(.1);
+const adamOpt = tf.train.adam(10);
 model.compile({
   optimizer: adamOpt,
   loss: 'meanSquaredError'
@@ -28,23 +28,19 @@ model.compile({
 async function fitModel(moveRecord) {
   console.log(moveRecord);
   for (var i = 0; i < moveRecord.length; i++) {
-     var posArr = moveRecord[i];
-     const expected = tf.oneHot(tf.tensor1d([posArr[3]], 'int32'), 3).cast('float32');
-     //console.log(expected);
-     posArr.pop();
-     posArr = tf.tensor2d([posArr]);
+     const expected = tf.oneHot(tf.tensor1d([getExpected(moveRecord[i])], 'int32'), 3).cast('float32');
+     posArr = tf.tensor2d([moveRecord[i]]);
      const h = await model.fit(posArr, expected, {
          batchSize: 3,
          epochs: 1
      });
-     //console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
+     console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
      expected.dispose();
      posArr.dispose();
   }
 }
 
 function makePrediction(input) {
-  input.pop();
   let inputs = tf.tensor2d([input]);
   const outputs = model.predict(inputs);
   return direction[outputs.argMax(1).dataSync()[0]];
